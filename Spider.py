1e7c15cc6b095ab27205a3e462ae3d5d3b156666
John <jiangh_wu@163.com>
2018-09-18 16:25:57 +0800

    true

---
+++ Spider.py
@@ -0,0 +1,160 @@
+import requests
+from ip_pool.auto_pool import get_ip
+from ip_pool.agents import agents
+from bs4 import BeautifulSoup
+import random
+import warnings
+import Logger
+import re
+
+warnings.filterwarnings("ignore")
+logger = Logger.get_logger()
+
+
+class Spider(object):
+    def __init__(self):
+        # self.ip = get_ip()
+        self.ip = {
+            'http': 'socks5://127.0.0.1:1086',
+            'https': 'socks5://127.0.0.1:1086'
+        }
+        self.url = 'https://www.google.com/search'
+        self.monitor = 0
+
+    def get_header(self):
+        return {
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-us;q=0.5,en;q=0.3',
+            'connection': 'keep-alive',
+            'user-agent': random.choice(agents)
+            }
+
+    def make_soup(self, payloads):
+        r = requests.get(
+            self.url,
+            params=payloads,
+            headers=self.get_header(),
+            proxies=self.ip,
+            verify=False,
+        )
+        r.encoding = "utf-8"
+        if r.status_code == 200:
+            return BeautifulSoup(r.text)
+        elif r.status_code == 503:
+            raise Exception('Error: {0} {1}'.format(r.status_code, r.reason))
+        else:
+            raise Exception('Error: {0} {1}'.format(r.status_code, r.reason))
+
+    def payloader(self, tp, content):
+        identifier = "q"
+        if tp in ["address"]:
+            return {
+                identifier: "Where is %s located in?" % content.split(';')[0],
+            }
+        if tp in ["email", "phone", "email and phone"]:
+            return {
+                identifier: "%s and email and phone" % content,
+            }
+        if tp in ["country"]:
+            return {
+                identifier: "What country is %s in?" % content.split(';')[0],
+            }
+        if tp in ["position"]:
+            return {
+                "q": "%s and (professor or researcher or scientist)" % content,
+            }
+
+    # Search the address by affiliation.
+    def get_address(self, affiliation):
+        try:
+            soup = self.make_soup(payloads=self.payloader("address", affiliation))
+            tag_results = soup.select("div[class='Z0LcW']")
+            return tag_results[0].getText() if len(tag_results) > 0 else ''
+        except Exception as e:
+            logger.error("%d | %s" % (self.monitor, str(e)))
+        finally:
+            return "ERROR"
+
+    def get_email_and_phone(self, keywords):
+        try:
+            soup = self.make_soup(payloads=self.payloader("email", keywords))
+            tag_results = soup.select("span[class='st']")
+            results = {str(tr)
+                           .replace(r"<em>", '')
+                           .replace(r"</em>", '')
+                           .replace(r"<wbr>", '')
+                           .replace(r"</wbr>", '') for tr in tag_results}
+
+            emailRegex = re.compile(r"""([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+(\.[a-zA-Z]{2,4}))""", re.VERBOSE)
+            emailFilterRegex = re.compile(r"""^[Ee]-?mail""")
+            phoneRegex = re.compile(
+                r"""([Pp]hone|[Mm]obile|[Tt]el)[,:]?[\s/]*(\+\s?[\d]+[\s/]?)?(\([\d\-. ]+\)[\s/]{0,2})*(\d+[/.-]?[\s/]?)*""",
+                re.VERBOSE)
+            phoneFilterRegex = re.compile(r"""([Pp]hone|[Mm]obile|[Tt]el)[,:]?\s*""")
+
+            email = str()
+            phone = str()
+            # 每一条摘要
+            for r in results:
+                pho = phoneRegex.search(r)
+                pho_no = phoneFilterRegex.sub('', pho.group()).strip() if pho is not None else ''
+                phone = pho_no if len(pho_no) > len(phone) else phone
+
+                ems = [emailFilterRegex.sub('', e).strip() for a in emailRegex.findall(r) for e in a]
+                for e in ems:
+                    email = e if len(e) > len(email) else email
+            return email, phone
+
+        except Exception as e:
+            logger.error("%d | %s" % (self.monitor, str(e)))
+        finally:
+            return "ERROR", "ERROR"
+
+    def get_country(self, affiliation):
+        try:
+            soup = self.make_soup(payloads=self.payloader("country", affiliation))
+            tag_results = soup.select("div[class='Z0LcW']")
+            return tag_results[0].getText() if len(tag_results) > 0 else ''
+        except Exception as e:
+            logger.error("%d | %s" % (self.monitor, str(e)))
+        finally:
+            return "ERROR"
+
+    def get_position(self, keywords):
+        try:
+            soup = self.make_soup(payloads=self.payloader("position", keywords))
+            tag_results = soup.select("span[class='st']")
+            results = {str(tr)
+                           .replace(r"<em>", '')
+                           .replace(r"</em>", '')
+                           .replace(r"<wbr>", '')
+                           .replace(r"</wbr>", '') for tr in tag_results}
+
+            associateProfessorRegex = re.compile('''[Aa]ssociate\s+[Pp]rofessor''')
+            assistantProfessorRegex = re.compile('''[Aa]ssistant\s+[Pp]rofessor''')
+            professorRegex = re.compile('''[Pp]rofessor''')
+            researcherRegex = re.compile('''[Rr]esearcher''')
+            scientistRegex = re.compile('''[Ss]cientist''')
+
+            # position需要设置优先级
+            for r in results:
+                if associateProfessorRegex.search(r):
+                    return 'Associate Professor'
+
+                if assistantProfessorRegex.search(r):
+                    return 'Assistant Professor'
+
+                if professorRegex.search(r):
+                    return "Professor"
+
+                if researcherRegex.search(r):
+                    return 'Researcher'
+
+                if scientistRegex.search(r):
+                    return 'Scientist'
+        except Exception as e:
+            logger.error("%d | %s" % (self.monitor, str(e)))
+        finally:
+            return "ERROR"
+
---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import pandas
+import time
+from Spider import Spider,logger
+import json
+import sys
+from multiprocessing import Process
+path_tag_csv = os.path.join(os.getcwd(), "CSVs", "remedy_0809.csv")
+path_dir_result = os.path.join(os.getcwd(), "result_file")
+
+mat = pandas.read_csv(path_tag_csv)
+affiliation_list = mat.iloc[:, 2].values.tolist()
+MAXSIZE = 5660
+
+
+def save_file(filename, list):
+    with open(filename, "a", encoding="utf-8") as f:
+        for l in list:
+            f.write(json.dumps(l) + "\n")
+
+
+def spider(begin, end):
+    path = os.path.join(path_dir_result, "%d_%d") % (begin, int(time.time()) % 1000)
+
+    with open(path, "w", encoding="utf-8") as f:
+        pass
+    sp = Spider()
+    result_list = []
+    for i in range(begin, end):
+        sp.monitor = i
+        country = sp.get_country(affiliation_list[i])
+        address = sp.get_address(affiliation_list[i])
+        item = (i, country, address)
+        result_list.append(item)
+        if i % 10 == 0 or i == end - 1:
+            save_file(path, result_list)
+            logger.info("%d | %s | %s" % item)
+
+
+if __name__ == '__main__':
+    begin = int(sys.argv[1])
+    end = int(sys.argv[2])
+    num_of_process = int(sys.argv[3])
+
+    quarter = round((end - begin) / num_of_process)
+
+    args = [(begin + i * quarter, begin + (1+i) * quarter) for i in range(num_of_process-1)]
+    args.append((begin + (num_of_process - 1) * quarter, end))
+
+    for a in args:
+        p = Process(target=spider, args=a)
+        p.start()
+        print(a)
+        time.sleep(5)
